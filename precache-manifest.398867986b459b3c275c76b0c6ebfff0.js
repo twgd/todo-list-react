c6e86a8899c2bd649971b89219041bd6e1052fee
twgd <twgd.huang@gmail.com>
2018-12-21 19:14:48 +0800

    Updates

---
+++ precache-manifest.398867986b459b3c275c76b0c6ebfff0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "70e51a5079889c2ac485",
+    "url": "/todo-list-react/static/js/runtime~main.70e51a50.js"
+  },
+  {
+    "revision": "f95cfe845fd036577401",
+    "url": "/todo-list-react/static/js/main.f95cfe84.chunk.js"
+  },
+  {
+    "revision": "aac75bbadb65a070ad02",
+    "url": "/todo-list-react/static/js/1.aac75bba.chunk.js"
+  },
+  {
+    "revision": "f95cfe845fd036577401",
+    "url": "/todo-list-react/static/css/main.7d0edef2.chunk.css"
+  },
+  {
+    "revision": "a6ce3e448a1d13f6fe6bcad6cacb6377",
+    "url": "/todo-list-react/index.html"
+  }
+];
